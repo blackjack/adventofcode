d60a79483cb625986991b11f65412b3f4bfcec6d
Oleksandr Senkovych <bjsenya@gmail.com>
2015-12-22 16:28:42 +0100

    Day 1 part 2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -5,12 +5,19 @@ fn main() {
     let file = File::open("input.txt").unwrap();
 
     let mut floor = 0;
+    let mut pos = 1;
+    let mut found_pos = false;
     for byte in file.bytes() {
         match byte.unwrap() as char {
             '(' => floor+=1,
             ')' => floor-=1,
             _   => {}
         }
+        if !found_pos && floor==-1 {
+            println!("Position that causes basement entering: {}",pos);
+            found_pos = true;
+        }
+        pos+=1;
     }
 
     println!("Result floor is: {}",floor);
