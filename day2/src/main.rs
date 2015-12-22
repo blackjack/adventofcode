cd881a4cae88a831e90740624b66dcf33bad18e6
Oleksandr Senkovych <bjsenya@gmail.com>
2015-12-22 16:47:13 +0100

    Day 2 part 2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -8,20 +8,27 @@ fn main() {
     let reader = BufReader::new(&file);
 
     let mut total = 0;
+    let mut ribbon = 0;
 
     for line in reader.lines() {
-        let l = line.unwrap();
-        if l.is_empty() { continue; }
-        let vec: Vec<&str> = l.split("x").collect();
+        let line = line.unwrap();
+        if line.is_empty() { continue; }
+        let mut vec: Vec<u32> = line.split("x").map(|s| s.parse::<u32>().unwrap()).collect();
 
         assert_eq!(vec.len(),3);
-        let l = vec[0].parse::<u32>().unwrap();
-        let w = vec[1].parse::<u32>().unwrap();
-        let h = vec[2].parse::<u32>().unwrap();
+        let l = vec[0];
+        let w = vec[1];
+        let h = vec[2];
 
         let extra = min(l*w,min(l*h,w*h));
         total+=2*l*w + 2*w*h + 2*h*l + extra;
+
+        vec.sort();
+        let r1 = vec[0];
+        let r2 = vec[1];
+
+        ribbon+=r1+r1+r2+r2+l*w*h;
     }
 
-    println!("Total paper required: {}",total);
+    println!("Total paper required: {}, total ribbon length: {}",total, ribbon);
 }
