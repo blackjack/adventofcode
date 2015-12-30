e7f347852e39b8a126fea7ebb4a67af1a21c7dfc
Oleksandr Senkovych <bjsenya@gmail.com>
2015-12-30 13:07:23 +0100

    Day 8

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -1,35 +1,35 @@
 use std::fs::File;
 use std::io::prelude::*;
 
-fn escape(s: &String) -> String {
-    let mut result: String = String::new();
-    if s.is_empty() { return "".to_string(); }
-
+fn escape<'a>(s: &[u8]) -> Vec<u8> {
+    let mut result: Vec<u8> = Vec::new();
     result.reserve(s.len());
-    let mut chars = s.chars().enumerate();
+
+    let mut chars = s.iter().enumerate();
 
     loop {
         let (idx,c) = match chars.next() {
             Some((idx,ch)) => (idx,ch),
             _ => break
         };
+        let c = *c as char;
 
         if c=='"' && (idx==0||idx==s.len()-1) { continue }
         if c=='\\' {
             match chars.next() {
-                Some((_,ch)) if ch!='x' => { result.push(ch); continue },
-                Some((_,ch)) if ch=='x' => {
-                    let (_,h1) = chars.next().unwrap();
-                    let (_,h2) = chars.next().unwrap();
+                Some((_,ch)) if *ch as char != 'x' => { result.push(*ch); continue },
+                Some((_,ch)) if *ch as char == 'x' => {
+                    let h1 = *chars.next().unwrap().1 as char;
+                    let h2 = *chars.next().unwrap().1 as char;
                     let xx = h1.to_digit(16).unwrap()*16 + h2.to_digit(16).unwrap();
-                    result.push( xx as u8 as char );
+                    result.push( xx as u8 );
                     continue;
                 },
                 _ => break,
             };
         }
 
-        result.push(c);
+        result.push(c as u8);
     }
 
     return result;
@@ -39,23 +39,19 @@ fn main() {
     let file = File::open("input.txt").unwrap();
     let reader = std::io::BufReader::new(file);
 
-    let mut out = File::create("output.txt").unwrap();
-
     let mut codelen = 0;
     let mut strlen = 0;
     for line in reader.lines() {
         let line = line.unwrap();
 
         codelen+=line.len();
-        let esc = escape(&line);
+        let esc = escape(line.as_bytes());
 
-        println!("Source: [{}], Escaped: [{}]",line,esc);
         strlen+=esc.len();
-        break;
     }
 
-    let s = "\"aaa\\\"aaa\"".to_string();
-    let e = escape(&s);
+    let s = "\"aaa\\\"aaa\"";
+    let e = escape(s.as_bytes());
     println!("A: {}, B: {}, D: {}",s.len(),e.len(),s.len()-e.len());
     println!("Total code chars: {}, total escaped chars: {}, diff: {}",codelen,strlen,codelen-strlen);
 }
@@ -64,31 +60,30 @@ fn main() {
 
 #[test]
 fn it_works() {
-    let strings: Vec<String> = vec!(
+    let strings = vec!(
         "\"\"",
         "\"abc\"",
         "\"aaa\\\"aaa\"",
         "\"\\x27\""
-    ).iter().map(|s| s.to_string()).collect();
+    );
 
     let l = &strings[0];
-    let esc = escape(&l);
-    println!("[{}] [{}]",l,esc);
+    let esc = escape(l.as_bytes());
     assert_eq!(l.len(),2);
     assert_eq!(esc.len(),0);
 
     let l = &strings[1];
-    let esc = escape(&l);
+    let esc = escape(l.as_bytes());
     assert_eq!(l.len(),5);
     assert_eq!(esc.len(),3);
 
     let l = &strings[2];
-    let esc = escape(&l);
+    let esc = escape(l.as_bytes());
     assert_eq!(l.len(),10);
     assert_eq!(esc.len(),7);
 
     let l = &strings[3];
-    let esc = escape(&l);
+    let esc = escape(l.as_bytes());
     assert_eq!(l.len(),6);
     assert_eq!(esc.len(),1);
 }
--- day8/src/test.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-
-import sys
-
-
-
-for line in sys.stdin:
-  print eval(line[:-1])
