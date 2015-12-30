b8cb73eea1ff7ea2b4497b7ef08aa4647ff357ee
Oleksandr Senkovych <bjsenya@gmail.com>
2015-12-30 13:49:27 +0100

    Day 8 part 2

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -1,8 +1,8 @@
 use std::fs::File;
 use std::io::prelude::*;
 
-fn escape<'a>(s: &[u8]) -> Vec<u8> {
-    let mut result: Vec<u8> = Vec::new();
+fn decode(s: &[u8]) -> Vec<char> {
+    let mut result: Vec<char> = Vec::new();
     result.reserve(s.len());
 
     let mut chars = s.iter().enumerate();
@@ -17,20 +17,37 @@ fn escape<'a>(s: &[u8]) -> Vec<u8> {
         if c=='"' && (idx==0||idx==s.len()-1) { continue }
         if c=='\\' {
             match chars.next() {
-                Some((_,ch)) if *ch as char != 'x' => { result.push(*ch); continue },
+                Some((_,ch)) if *ch as char != 'x' => { result.push(c); continue },
                 Some((_,ch)) if *ch as char == 'x' => {
                     let h1 = *chars.next().unwrap().1 as char;
                     let h2 = *chars.next().unwrap().1 as char;
                     let xx = h1.to_digit(16).unwrap()*16 + h2.to_digit(16).unwrap();
-                    result.push( xx as u8 );
+                    result.push( xx as u8 as char );
                     continue;
                 },
                 _ => break,
             };
         }
 
-        result.push(c as u8);
+        result.push(c);
     }
+
+    return result;
+}
+
+fn encode(s: &[u8]) -> Vec<char> {
+    let mut result: Vec<char> = Vec::new();
+    result.reserve(s.len());
+
+    result.push('"');
+    for c in s.iter() {
+        let ch = *c as char;
+        if ch=='"' || ch=='\\' {
+            result.push('\\');
+        }
+        result.push(ch);
+    }
+    result.push('"');
 
     return result;
 }
@@ -40,26 +57,27 @@ fn main() {
     let reader = std::io::BufReader::new(file);
 
     let mut codelen = 0;
-    let mut strlen = 0;
+    let mut declen = 0;
+    let mut enclen = 0;
     for line in reader.lines() {
         let line = line.unwrap();
 
-        codelen+=line.len();
-        let esc = escape(line.as_bytes());
+        let d = decode(line.as_bytes());
+        let e = encode(line.as_bytes());
 
-        strlen+=esc.len();
+        codelen += line.len();
+        declen += d.len();
+        enclen += e.len();
     }
 
-    let s = "\"aaa\\\"aaa\"";
-    let e = escape(s.as_bytes());
-    println!("A: {}, B: {}, D: {}",s.len(),e.len(),s.len()-e.len());
-    println!("Total code chars: {}, total escaped chars: {}, diff: {}",codelen,strlen,codelen-strlen);
+    println!("Total code chars: {}, total decoded chars: {}, total encoded chars: {}, diff(dec): {}, diff(enc): {}",
+             codelen,declen,enclen,codelen-declen,enclen-codelen);
 }
 
 
 
 #[test]
-fn it_works() {
+fn test_decode() {
     let strings = vec!(
         "\"\"",
         "\"abc\"",
@@ -68,22 +86,44 @@ fn it_works() {
     );
 
     let l = &strings[0];
-    let esc = escape(l.as_bytes());
-    assert_eq!(l.len(),2);
+    let esc = decode(l.as_bytes());
     assert_eq!(esc.len(),0);
 
     let l = &strings[1];
-    let esc = escape(l.as_bytes());
-    assert_eq!(l.len(),5);
+    let esc = decode(l.as_bytes());
     assert_eq!(esc.len(),3);
 
     let l = &strings[2];
-    let esc = escape(l.as_bytes());
-    assert_eq!(l.len(),10);
+    let esc = decode(l.as_bytes());
     assert_eq!(esc.len(),7);
 
     let l = &strings[3];
-    let esc = escape(l.as_bytes());
-    assert_eq!(l.len(),6);
+    let esc = decode(l.as_bytes());
     assert_eq!(esc.len(),1);
+}
+
+#[test]
+fn test_encode() {
+    let strings = vec!(
+        "\"\"",
+        "\"abc\"",
+        "\"aaa\\\"aaa\"",
+        "\"\\x27\""
+    );
+
+    let l = &strings[0];
+    let esc = encode(l.as_bytes());
+    assert_eq!(esc.len(),6);
+
+    let l = &strings[1];
+    let esc = encode(l.as_bytes());
+    assert_eq!(esc.len(),9);
+
+    let l = &strings[2];
+    let esc = encode(l.as_bytes());
+    assert_eq!(esc.len(),16);
+
+    let l = &strings[3];
+    let esc = encode(l.as_bytes());
+    assert_eq!(esc.len(),11);
 }
